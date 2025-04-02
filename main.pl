cd8f51d8a1fc68d802d50736a6bbad236914d0ac
Juan Fernando Cano <173503745+JuanF-Cano@users.noreply.github.com>
2025-04-01 20:04:07 -0500

    reglas de abuelos y hermanos

--- main.pl
+++ main.pl
@@ -6,3 +6,20 @@ madre_de(mujer(jacqueline), [mujer(marge), mujer(patty), mujer(selma)]).
 madre_de(mujer(marge), [hombre(bart), mujer(lisa), mujer(maggie)]).
 madre_de(mujer(selma), [hijo(ling)]).
 
+abuelo_de(X, Y) :-
+    padre_de(X, Z), (padre_de(Z, Y); madre_de(Z, Y)).
+
+abuela_de(X, Y) :-
+    madre_de(X, Z), (padre_de(Z, Y); madre_de(Z, Y)).
+
+hermano_de(X, Y) :-
+    (padre(hombre(_), Hijos); madre(hombre(_), Hijos)),
+    member(hombre(X), Hijos),
+    (member(hombre(Y), Hijos); member(mujer(Y), Hijos)),
+    X \= Y.
+
+hermana_de(X, Y) :-
+    (padre(mujer(_), Hijos); madre(mujer(_), Hijos)),
+    member(mujer(X), Hijos),
+    (member(hombre(Y), Hijos); member(mujer(Y), Hijos)),
+    X \= Y.
