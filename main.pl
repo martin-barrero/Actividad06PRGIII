1eb3b6d188e904cb65c4707f46c3875b29ff47f6
martin-barrero <martin.barrero@utp.edu.co>
2025-04-01 19:20:09 -0500

    add padre_de y madre_de.pl

--- main.pl
+++ main.pl
@@ -1,1 +1,7 @@
-% Mi primer archivo en Prolog
+padre_de(hombre(homero), [hombre(bart), mujer(lisa), mujer(maggie)]).
+padre_de(hombre(abraham), [hombre(herbert), hombre(homero)]).
+padre_de(hombre(clancy), [mujer(marge), mujer(patty), mujer(selma)]).
+madre_de(mujer(mona), [hombre(homero)]).
+madre_de(mujer(jacqueline), [mujer(marge), mujer(patty), mujer(selma)]).
+madre_de(mujer(marge), [hombre(bart), mujer(lisa), mujer(maggie)]).
+madre_de(mujer(selma), [hijo(ling)]).
